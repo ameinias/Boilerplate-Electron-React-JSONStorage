538f1a8bdc927049873bc34cec1b2d1a0df5b002
ameinias <gillian@blekkenhorst.ca>
2025-06-27 10:21:24 -0400

    this one works but I can't get routing working. Started a fresh repo with updated dependancies to see if I can get that working better, but

---
+++ ClientApp/components/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserProfile = () => {
+
+
+    return (
+        <div>
+            <p>user profile</p>
+        </div>
+    )
+
+}
+
+export default UserProfile
