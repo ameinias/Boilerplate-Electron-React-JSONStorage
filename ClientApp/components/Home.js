e2576e1f30d2b76543d7202d029f33f6fa42239b
ameinias <gillian@blekkenhorst.ca>
2025-06-27 10:34:17 -0400

    Revert "Merge branch 'main' of https://github.com/ameinias/archive-BERJ"
    This reverts commit 613ed16844707e9fc704c45a3811c9506945b089, reversing
    changes made to 538f1a8bdc927049873bc34cec1b2d1a0df5b002.

--- ClientApp/components/Home.js
+++ ClientApp/components/Home.js
@@ -1,89 +1,56 @@
 import React, {useState, useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
-import {loadSavedData} from "../renderer.js";
+import InputGroup from "react-bootstrap/InputGroup";
+import {loadSavedData, saveDataInStorage} from "../renderer.js";
 import List from "./List";
 import Search from "./Search";
 const { ipcRenderer } = require("electron");
 const { HANDLE_FETCH_DATA, HANDLE_SAVE_DATA, HANDLE_REMOVE_DATA, HANDLE_EDIT_DATA } = require("../../utils/constants")
 
 const Home = () => {
-  //const [pieView, changeView] = useState(true)
-  //const [expenses, setExpenses] = useState([]);
-    const [val, setVal] = useState("");
-    const [dbItems, setItems] = useState([]);
+  const [val, setVal] = useState("");
+  const [itemsToTrack, setItems] = useState([]);
 
-  // Keep a separate reference to all the expenses that match the current filter
-  const [filteredExpenses, setFilteredExpenses] = useState([]);
-
-  // Keep track of filters / single category here -- it will be used by both ExpensesList and BudgetProgress comps
-  const [categoryFilter, setCategoryFilter] = useState('All')
-
-
-
-  // Grab the user's saved expenses after the app loads
+  // Grab the user's saved itemsToTrack after the app loads
   useEffect(() => {
     loadSavedData();
   }, []);
 
-  // Update expenses
-  useEffect(() => {
-    const filtered = dbItems.filter(exp => {
-      // const expDate = new Date(exp.date)
-      // const year = getYear(expDate);
-      // const month = getMonth(expDate);
-      return (categoryFilter === "All" || categoryFilter === exp.category)
-        // && getYear(expDate) === calYear
-        // && getMonth(expDate) === calMonth
-    })
-    // console.log("NEW FILTERED EXPENSES:", filtered)
-    setFilteredExpenses(filtered)
-    // Listen for new Expenses, change in month or filter
-  }, [categoryFilter,  dbItems]);
-
-
   // Listener functions that receive messages from main
   useEffect(() => {
-    ipcRenderer.on(HANDLE_SAVE_DATA, handleNewExpense);
+    ipcRenderer.on(HANDLE_SAVE_DATA, handleNewItem);
     // If we omit the next step, we will cause a memory leak and & exceed max listeners
     return () => {
-      ipcRenderer.removeListener(HANDLE_SAVE_DATA, handleNewExpense);
+      ipcRenderer.removeListener(HANDLE_SAVE_DATA, handleNewItem);
     }
   });
   useEffect(() => {
-    ipcRenderer.on(HANDLE_FETCH_DATA, handleReceiveExpensesFromMain);
+    ipcRenderer.on(HANDLE_FETCH_DATA, handleReceiveData);
     return () => {
-      ipcRenderer.removeListener(HANDLE_FETCH_DATA, handleReceiveExpensesFromMain);
+      ipcRenderer.removeListener(HANDLE_FETCH_DATA, handleReceiveData);
+    }
+  });
+  useEffect(() => {
+    ipcRenderer.on(HANDLE_REMOVE_DATA, handleReceiveData);
+    return () => {
+      ipcRenderer.removeListener(HANDLE_REMOVE_DATA, handleReceiveData);
     }
   });
 
-  // Receives all user expenses from main
-  const handleReceiveExpensesFromMain = (event, data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log("renderer received data from main:", data.message);
-      setItems([...data.message]);
-    }
+  // Receives itemsToTrack from main and sets the state
+  const handleReceiveData = (event, data) => {
+    setItems([...data.message]);
   };
 
-  // Receives a new expense from main
-  const handleNewExpense = (event, data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log("renderer received data from main:", data.message)
-      setItems([...dbItems, data.message])
-    }
+  // Receives a new item back from main
+  const handleNewItem = (event, data) => {
+    setItems([...itemsToTrack, data.message])
   }
 
-  // // Toggle between Pie and List views
-  // const handleChangeView = () => {
-  //   if (!pieView) {
-  //     // Going into Pie View we want to view all Categories
-  //     setCategoryFilter("All")
-  //   }
-  //   changeView(!pieView)
-  // }
+  // Manage state and input field
+  const handleChange = (e) => {
+    setVal(e.target.value)
+  }
 
   // Send the input to main
   const addItem = (input) => {
@@ -116,7 +83,7 @@ const Home = () => {
       {itemsToTrack.length ? (
         <List itemsToTrack={itemsToTrack} />
       ) : (
-        <p>Add an item fsfd to get started</p>
+        <p>Add an item to get started</p>
       )}
     </div>
   )
